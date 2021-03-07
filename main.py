db39c1da251d0790acda22a38d7607dc26d6491c
dawid-wol <bigcoconut01@gmail.com>
2021-03-07 17:47:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Zad1
+sporty = ["pilkanozna","siatkowka","koszykowka"]
+print(sporty)
+list.reverse(sporty)
+print(sporty)
+sporty.append("wioslarstwo")
+sporty.append("skokwdal")
+print(sporty)
+#Zad2
